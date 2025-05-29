09e813e7de70b14cc44fd43dc2d8829af6c7c1f3
Rodrigo Antonio Silveira Junior <rodrigo.asjunior@senacsp.edu.br>
2025-05-28 22:07:01 -0300

    Minha primeira rota em Nodejs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const express = require('express'); //deixar padrão que o servidor web inicie automaticamente//
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.write("Hello Word!");
+    res.end();
+});
+
+app.listen(8080);
+
