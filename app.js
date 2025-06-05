e8a529f965f6771696847b92f5dd53664310ad63
Rodrigo Antonio Silveira Junior <rodrigo.asjunior@senacsp.edu.br>
2025-06-04 22:10:54 -0300

    extensão myqsl2 e cominicação com o banco de dados

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express'); //deixar padrão que o servidor web inicie automaticamente//
-
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro){
+    if(erro){
+        console.error('Erro ao conectar ao banco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso');
+});
 
 app.get("/", function(req, res){
-    res.write("Hello Word!");
+    res.write("Ola mundo!");
     res.end();
 });
 
