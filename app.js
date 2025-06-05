1e080ec39c7dafffa319ccfff95d3d53179834f1
Rodrigo Antonio Silveira Junior <rodrigo.asjunior@senacsp.edu.br>
2025-06-05 20:11:45 -0300

    instalação e configuração do handlebars

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express'); //deixar padrão que o servidor web inicie automaticamente//
 const app = express();
+const { engine } = require('express-handlebars');
+
 const mysql = require('mysql2');
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const conexao = mysql.createConnection({
     host: 'localhost',
@@ -18,10 +24,10 @@ conexao.connect(function(erro){
     console.log('Conexão com o banco de dados estabelecida com sucesso');
 });
 
-app.get("/", function(req, res){
-    res.write("Ola mundo!");
-    res.end();
-});
+app.get('/', (req, res) => {;
+    res.render('teste');
+}
+);
 
 app.listen(8080);
 
